f8f319645d5d78246d5f1b28d8421878d7ba78ff
alfonso <martalf1987@gmail.com>
2020-09-15 23:07:11 -0300

    adecuacion de relaciones

--- tests/ApiAudioPhoneUserTest.php
+++ tests/ApiAudioPhoneUserTest.php
@@ -1,52 +1,34 @@
 <?php
 
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 use App\Apiaudiophonemodels\ApiAudiophoneUser;
 
-class ApiAudioPhoneUserTest extends TestCase
+class ApiaudiophoneUserTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Verificar usuario en la base de datos.
      *
      * @return void
-     *
-     * Anotacion para no colocar el prefijo tes en el nombre de las funciones.
      * @test
-    */
-    public function enviar_datos_de_usuario_desde_el_formulario_de_registro_en_formato_json()
-    {
-        //con el make() creas instancias de la clase pero no se graban en la base de datos, para grabar usas el create()
-        $apiaudiophoneuserfactory = factory(ApiAudiophoneUser::class)->make();
-
-        //envío del JSON vía post al controlador
-        $requestjson = $this->json('post', route('user.store'), ['apiaudiophoneuserfactory' => $apiaudiophoneuserfactory]);
-
-        /*
-         * para poder validar que en realidad viaja el json, es necesario hacer un dd($request->all) del lado del controlador
-         * en este caso la prueba fue satiscatoria.
-        */
-    }
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     *
-     * Anotacion para no colocar el prefijo tes en el nombre de las funciones.
-     * @test
-    */
-    public function crear_apiaudiophoneuser()
+     */
+    public function create_user_in_apiaudiophoneuesr_table()
     {
 
-        /*
-         * para que se logre la aserción el response del controlador debe devolver lo que espera la prueba
-         * en este caso es una prueba puntual, para pruebas dinamicas, usar postman
-        */
-        $this->json('post', route('user.store'), [
-            'apiaudiophoneusers_fullname' => 'Alfonso Martinez',
-            'apiaudiophoneusers_email' => 'a@a.com',
-            'apiaudiophoneusers_password' => '12345678'
-        ])->seeJson([
-            'ok' => true
-        ]);
+    	/*$apiaudiophoneuser = factory(ApiAudiophoneUser::class)->make([
+
+    		'apiaudiophoneusers_fullname' => $faker->name,
+    		'apiaudiophoneusers_email' => $faker->unique()->freeEmail,
+    		'apiaudiophoneusers_password' => app('hash')->make(Str::random(5))
+    	]);
+
+    	dd($apiaudiophoneuser);*/
+
+
+    	//$response = $this->call('POST', 'user.show', ['stringsearch' => 'a@a.com']); 
+
+    	//$this->assertEquals(200, $response->status());
+
+    	//$this->seeInDatabase('apiaudiophoneusers', ['apiaudiophoneusers_email' => 'a@a.com']);
     }
 }
